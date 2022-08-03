81e7f4615159af42be85bb757efa888e17b6a10c
Charlie-XIAO <yx2436@nyu.edu>
2022-08-03 18:03:00 +0800

    implemented Node2Vec class slight modifications on DeepWalk class to keep accordance with Node2Vec

--- embedders/previous_works/Node2Vec.py
+++ embedders/previous_works/Node2Vec.py
@@ -3,23 +3,27 @@ from gensim.models import Word2Vec
 from embedders.previous_works.RandomWalker import RandomWalker
 
 ### ========== ========== ========= ========== ========== ###
-### CLASS DEEP WALK ###
+### CLASS NODE TO VEC ###
 ### ========== ========== ========= ========== ========== ###
-class DeepWalk:
+class Node2Vec:
 
-    def __init__(self, graph, walk_length, num_walks, workers=1):
+    def __init__(self, graph, walk_length, num_walks, p=1, q=1, workers=1, use_rejection_sampling=False):
         """
         :param self:
         :param graph:
         :param walk_length:
         :param num_walks:
+        :param p:
+        :param q:
         :param workers:
+        :param use_rejection_sampling:
         :return: None
         """
         self.graph = graph
-        self.w2v_nodel = None
         self._embeddings = {}
-        self.walker = RandomWalker(graph, p=1, q=1)
+        self.walker = RandomWalker(graph, p=p, q=q, use_rejection_sampling=use_rejection_sampling)
+        print("Preprocessing transition probabilities...")
+        self.walker.preprocess_transition_probs()
         self.sentences = self.walker.simulate_walks(num_walks=num_walks, walk_length=walk_length, workers=workers, verbose=1)
 
     def train(self, embed_size=128, window_size=5, workers=3, iter=5, **kwargs):
@@ -36,14 +40,14 @@ class DeepWalk:
         kwargs["min_count"] = kwargs.get("min_count", 0)
         kwargs["vector_size"] = embed_size
         kwargs["sg"] = 1  # skip gram
-        kwargs["hs"] = 1  # deepwalk use Hierarchical Softmax
+        kwargs["hs"] = 0  # node2vec does not use Hierarchical Softmax
         kwargs["workers"] = workers
         kwargs["window"] = window_size
         kwargs["epochs"] = iter
         print("Learning embedding vectors...")
         model = Word2Vec(**kwargs)
         print("Learning embedding vectors done.")
-        self.w2v_nodel = model
+        self.w2v_model = model
         return model
     
     def get_embeddings(self):
@@ -51,10 +55,10 @@ class DeepWalk:
         :param self:
         :return: embedding
         """
-        if self.w2v_nodel is None:
+        if self.w2v_model is None:
             print("Error: model not trained.")
             return {}
         self._embeddings = {}
         for word in self.graph.nodes():
-            self._embeddings[word] = self.w2v_nodel.wv[word]
+            self._embeddings[word] = self.w2v_model.wv[word]
         return self._embeddings
