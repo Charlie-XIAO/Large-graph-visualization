9dbc98fe0e4660bcaca2e6bdd385f4800625a8bc
Charlie-XIAO <yx2436@nyu.edu>
2022-08-06 15:19:57 +0800

    select nodes of highest degrees instead of random selection - method: priority queue STL library

--- embedders/ShortestPath.py
+++ embedders/ShortestPath.py
@@ -1,6 +1,7 @@
+from turtle import position
 import numpy as np
 import networkx as nx
-import math
+import heapq
 
 ### ========== ========== ========= ========== ========== ###
 ### CLASS SHORTEST PATH ###
@@ -22,15 +23,28 @@ class ShortestPath:
         :param embed_size DEFAULT 128:
         :return: embeddings
         """
-        unconnected = self.graph.number_of_edges()
-        X = np.random.choice(list(self.graph.nodes()), size=embed_size)
+        # Select nodes of top degrees as base nodes
+        # Method: using priority queue STL library, runs O(nlogn) time and takes O(1) space
+        # Alternatively, randomly select base nodes using:
+        # X = np.random.choice(list(self.graph.nodes()), size=embed_size)
+        pq = []
+        heapq.heapify(pq)
+        degrees = [(d[1], d[0]) for d in self.graph.degree]  # (degree, node)
+        for i in range(self.graph.number_of_nodes()):
+            heapq.heappush(pq, degrees[i])
+            if len(pq) > embed_size:
+                heapq.heappop(pq)
+        X = np.array([heapq.heappop(pq)[1] for _ in range(embed_size)])
+
+        # Generate node embeddings
         self._embeddings = {}
+        edgecount = self.graph.number_of_edges()
         for source in self.graph.nodes():
-            position = []
+            node_embedding = []
             for target in X:
                 try:
-                    position.append(nx.shortest_path_length(self.graph, source=source, target=target))
+                    node_embedding.append(nx.shortest_path_length(self.graph, source=source, target=target))
                 except:
-                    position.append(unconnected)
-            self._embeddings[source] = position
+                    node_embedding.append(edgecount)
+            self._embeddings[source] = node_embedding
         return self._embeddings
